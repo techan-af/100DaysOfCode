b90413fb3beb43bc2db09b8cfa74b20a6ba8c005
Chetan Singh <96533473+techan-af@users.noreply.github.com>
2022-05-23 11:45:27 +0530

    Made a brand name generator from Python

---
+++ brand_name_gen.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in.")
+pet_name = input("What pet animal did you have?")
+print("Your band name is "+ str(city) + " " + str(pet_name))
