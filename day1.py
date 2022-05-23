96fd0782b66a9fce01c4cefe2c1f0068ca9c065d
Chetan Singh <96533473+techan-af@users.noreply.github.com>
2022-05-23 11:46:33 +0530

    Practiced some basic concepts

---
+++ day1.py
@@ -0,0 +1,22 @@
+# print("hello world!")
+#to tell the computer to print the string
+print("Day 1 - Python print function")
+print("The function is declared like this:")
+print("print('what to print')")
+#use different print methods to get output in different lines
+print("Hello world!\nHello world!")
+#does the same thing using single statement
+# print("Hello "+"Python")
+# or concatenate by print("Hello" + " " + "Python")
+#Python spaces are very sensitive. Indentation error
+print('Day - 1 String Manipulation\nString concatenation is done with the "+" sign.\ne.g. print("Hello "+"world")\nNew likes can be created with a backslash and n.')
+
+#input() wil get user input in console
+# print("Hello "+ input("What is your name?"))
+
+#Exercise Find the number of letters in a name
+
+name = input("What is your name? ")
+
+number_of_letters = len(name)
+print(number_of_letters)
